fb84a381c04331d1d9e5ceb097072884591f413d
Davit Karapetian <karapetiandav@gmail.com>
2018-10-06 12:27:52 +0300

    api service

---
+++ app/src/main/java/ru/karapetiandav/binance/api/ApiServer.kt
@@ -0,0 +1,26 @@
+package ru.karapetiandav.binance.api
+
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import ru.karapetiandav.binance.models.ExchangeInfo
+
+
+interface ApiServer {
+    @GET("exchangeInfo")
+    fun getExchangeInfo(): Single<ExchangeInfo>
+
+    companion object {
+        fun create(): ApiServer {
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://api.binance.com/api/v1/")
+                    .build()
+
+            return retrofit.create(ApiServer::class.java)
+        }
+    }
+}
